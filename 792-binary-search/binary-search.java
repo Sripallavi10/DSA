edad54beb7d72378eca895ce7802598c27441dc6
Guru Sri Pallavi <gurusripallavi05@gmail.com>
2026-03-19 21:40:39 +0530

    Time: 0 ms (100.00%) | Memory: 48.6 MB (25.58%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -2,10 +2,10 @@ class Solution {
     public int search(int[] nums, int target) {
         int low=0,high=nums.length-1;
         while(low<=high){
-            int mid=(low+high)/2;
-            if(nums[mid]==target){
+            int mid=(high+low)/2;
+            if(target==nums[mid]){
                 return mid;
-            }else if(target>nums[mid]) {
+            }else if(nums[mid]<target){
                 low=mid+1;
             }else {
                 high=mid-1;
