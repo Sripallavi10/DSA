581b87922288dc9b1c5f88c3f5e8778533af5748
Guru Sri Pallavi <gurusripallavi05@gmail.com>
2026-04-01 07:48:55 +0530

    Time: 3 ms (8.25%) | Memory: 43.1 MB (23.66%) - LeetSync

---
+++ 812-rotate-string/rotate-string.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        int n=s.length();
+        for(int i=0;i<n;i++){
+        if(s.equals(goal)){
+            return true;
+        }
+        char last=s.charAt(n-1);
+        s=last+s.substring(0,n-1);
+        }
+    return false;
+    }
+}
